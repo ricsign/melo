f11e86e6008848b31ebdf86faef832a58b90e987
ricsign <ricsign123@gmail.com>
2022-09-17 23:00:51 -0400

    deployment 2

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -3,13 +3,18 @@ import React, { useState } from "react";
 function Searchbar({ setSearchedSongs }) {
   let submitSearch = async (e) => {
     if (e.key !== "Enter") return;
-
+    
     // api calls to fetch data from user input
-    const songs = await fetch("http://localhost:8000/songs").then((response) =>
-      response.json()
-    );
+    let songs = await fetch("https://api.jsonbin.io/v3/b/"+process.env.REACT_APP_JSON_BIN_ID,{
+      method: "GET",
+      headers: {
+        'X-Master-Key': process.env.REACT_APP_JSON_X_MASTER_KEY,
+        'X-Access-Key': process.env.REACT_APP_JSON_X_ACCESS_KEY,
+      }
+    }).then((response) => response.json())
+    .then((data) => data.record.songs);
+    
     const userInput = e.target.value.toLowerCase();
-    // const notExist = true;
 
     setSearchedSongs([]);
     for (let song of songs) {
@@ -18,20 +23,15 @@ function Searchbar({ setSearchedSongs }) {
         (song.letternotes.includes(userInput) ||
         song.numbernotes.includes(userInput) ||
         song.name.toLowerCase().includes(userInput) ||
-        song.category.toLowerCase().includes(userInput.toLowerCase()))
+        song.category.toLowerCase().includes(userInput))
       ) {
         setSearchedSongs((songs) => {
-          // notExist = false;
           return [...songs, song];
         });
         
       }
     }
 
-    // if(notExist){
-    //   alert('No songs found')
-    //   return
-    // }
   };
 
   return (
